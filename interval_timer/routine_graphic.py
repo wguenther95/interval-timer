4aac512e41053af483b7f17c94145f4e5a34f322
Wes Guenther <guenthwd@miamioh.edu>
2020-10-02 11:35:40 -0500

    feat: add class for graphical cycle elements

--- interval_timer/routine_graphic.py
+++ interval_timer/routine_graphic.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QLineEdit
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QLineEdit,
+                             QScrollArea, QComboBox, QSpacerItem, QSizePolicy)
 from PyQt5.QtCore import Qt
 import os
 import sys
@@ -40,3 +41,39 @@ class RoutineWidget(QWidget):
     def update_routine(self):
         self.routine.update(self.name.text(), 1, 10, 30)
         print(self.routine.name)
+
+
+class CycleWidget(QWidget):
+    def __init__(self, name):
+        super().__init__()
+
+        sets_label = QLabel(name)
+        self.sets = QComboBox()
+        self.sets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        low_time_label = QLabel("Low")
+        self.low_time = QLineEdit('10')
+
+        high_time_label = QLabel("High")
+        self.high_time = QLineEdit('60')
+
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(sets_label)
+        top_layout.addWidget(self.sets)
+
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addWidget(low_time_label)
+        bottom_layout.addWidget(self.low_time)
+        bottom_layout.addWidget(high_time_label)
+        bottom_layout.addWidget(self.high_time)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(bottom_layout)
+
+        spacer_item = QSpacerItem(0, 50, QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+
+class CycleScrollArea(QScrollArea):
+    def __init__(self):
+        super().__init__()
